927e1f5ac2c2ca26d0c34bb47742443c953ed21a
張建文 <chang180@gmail.com>
2021-03-09 19:15:17 +0800

    Laravel breeze set up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,26 @@
         </style>
     </head>
     <body class="antialiased">
-        New Project
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 text-xl bg-white">
+                
+                這是一個新專案：
+<x-input type="text" id="test" placeholder="這是個測試輸入欄" />
+            </div>
+        </div>
     </body>
 </html>
